972e13d7a4460ab5ed2bd0f63066686052f7a957
Steve Song <sshtel@gmail.com>
2016-07-04 18:53:59 -0700

    fix bashrc.sh parameter bug

--- bashrc.sh
+++ bashrc.sh
@@ -6,15 +6,15 @@ echo "##### MY CUSTOM FUNCTIONS #####" >> ~/.bashrc
 echo "###############################" >> ~/.bashrc
 
 echo "function greps(){" >> ~/.bashrc
-echo "    grep -rn $1 ./" >> ~/.bashrc
+echo "    grep -rn \$1 ./" >> ~/.bashrc
 echo "}" >> ~/.bashrc
 echo "function finds(){" >> ~/.bashrc
-echo "    find ./ -name $1" >> ~/.bashrc
+echo "    find ./ -name \$1" >> ~/.bashrc
 echo "}" >> ~/.bashrc
 echo "function grepc(){" >> ~/.bashrc
-echo "    find . -name '*.cpp' | xargs grep -rn --color=auto $1" >> ~/.bashrc
-echo "    find . -name '*.c' | xargs grep -rn --color=auto $1" >> ~/.bashrc
-echo "    find . -name '*.h' | xargs grep -rn --color=auto $1" >> ~/.bashrc
+echo "    find . -name '*.cpp' | xargs grep -rn --color=auto \$1" >> ~/.bashrc
+echo "    find . -name '*.c' | xargs grep -rn --color=auto \$1" >> ~/.bashrc
+echo "    find . -name '*.h' | xargs grep -rn --color=auto \$1" >> ~/.bashrc
 echo "}" >> ~/.bashrc
 
 source ~/.bashrc
